2f9456d5848b883e6452b4501669854b498a9566
이동영 <leedy2491@ajou.ac.kr>
2021-02-27 15:29:28 +0900

    Thank you for visiting dev2d0's Github.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
     <div>
-      안녕
+      안녕하세요
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
